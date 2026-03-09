87a845bb91191eff5c2f697c470395b0fc9702c6
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-03-09 11:40:48 +0800

    aw

--- app/Http/Middleware/CheckStationPC.php
+++ app/Http/Middleware/CheckStationPC.php
@@ -21,6 +21,7 @@ class CheckStationPC
     {
         $station = $request->route('station');
         $ip = $request->ip();
+
         try {
             $decrypted = Crypt::decryptString($station);
         } catch (DecryptException $e) {
